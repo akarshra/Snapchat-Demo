d8799569eeca7fec00d525b76d3439ecc1935dd2
Akarsh Raj <akarshsrivastava322@gmail.com>
2026-08-07 09:28:24 +0530

    Update ALLOWED_HOSTS in settings.py to allow all domains for Vercel deployment

--- snapchat/settings.py
+++ snapchat/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-e3d3e5_g2jgwr2y3#57t=_j(7slqldga1o3yl1gloq*@!%2j0s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
